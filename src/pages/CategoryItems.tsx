a7c062509fa4647cab63af7a1eef64438524f8c2
Atul Singh <atulkumar2316singh@gmail.com>
2022-10-16 08:26:06 +0530

    Worked on the category items page.

--- src/pages/CategoryItems.tsx
+++ src/pages/CategoryItems.tsx
@@ -1,5 +1,11 @@
-import { Box, Heading } from "@chakra-ui/react";
-import { Fragment } from "react";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+} from "@chakra-ui/react";
 import CategoryItemCard from "../components/CategoryItem/CategoryItemCard";
 import NotificationBox from "../components/Shared/NotificationBox";
 import PageHeader from "../components/Shared/PageHeader";
@@ -12,27 +18,55 @@ const CategoryItems = () => {
   return (
     <>
       <PageHeader leftChild={<PageHeading heading="Items" />} />
-      {categoryForms.map((categoryForm) => {
-        return (
-          <Fragment key={categoryForm.id}>
-            <Heading size="xl" my={4}>
-              {categoryForm.categoryName}
-            </Heading>
-            {categoryForm.items.map((categoryItem) => (
-              <Box my={4} key={categoryItem.id}>
-                <CategoryItemCard
-                  titleId={categoryForm.titleId}
-                  categoryItem={categoryItem}
-                  categoryId={categoryForm.id}
-                />
+
+      <Accordion defaultIndex={[0]} allowMultiple>
+        {categoryForms.map((categoryForm) => {
+          return (
+            <AccordionItem key={categoryForm.id} my={4} border="none">
+              <Box>
+                <AccordionButton
+                  bg="#E2E8F0"
+                  borderRadius={10}
+                  _expanded={{ bg: "gray.200", border: "none" }}
+                >
+                  <Box
+                    flex="1"
+                    textAlign="left"
+                    py={3}
+                    fontSize={18}
+                    fontWeight={600}
+                    color="gray.700"
+                  >
+                    {categoryForm.categoryName}
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
               </Box>
-            ))}
-            {categoryForm.items.length === 0 && (
-              <NotificationBox type="error" message="No items added yet." />
-            )}
-          </Fragment>
-        );
-      })}
+
+              <AccordionPanel>
+                {categoryForm.items.map((categoryItem) => (
+                  <Box my={4} key={categoryItem.id}>
+                    <CategoryItemCard
+                      titleId={categoryForm.titleId}
+                      categoryItem={categoryItem}
+                      categoryId={categoryForm.id}
+                    />
+                  </Box>
+                ))}
+                {categoryForm.items.length === 0 && (
+                  <Box my={4}>
+                    <NotificationBox
+                      type="error"
+                      message="No items added yet."
+                    />
+                  </Box>
+                )}
+              </AccordionPanel>
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
+
       {categoryForms.length === 0 && (
         <NotificationBox type="error" message="Nothing to show." />
       )}
